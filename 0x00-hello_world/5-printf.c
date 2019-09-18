7562ba23dc5ef09990df8a33ff00d0714b7242d7
mmanumos <mosqueramanuel5@gmail.com>
2019-09-18 21:11:20 +0000

    program that prints exactly with proper grammar, but the outcome is a piece of art

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints required text
+ *
+ *Return: value 0
+ */
+
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
