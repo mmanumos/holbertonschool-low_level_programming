ce3e148146670cac064a12b9401bfd81183da328
mmanumos <mosqueramanuel5@gmail.com>
2019-09-19 18:36:06 +0000

    Fix 3 - Complete the source code in order to print the last digit of the number stored in the variable n.

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ int main(void)
 	}
 	else if (s == 0)
 	{
-		printf("Last digit of %d is 0 and is 0\n", n);
+		printf("Last digit of %d is %d and is 0\n", n, s);
 	}
 	else if (s < 6 && s != 0)
 	{
