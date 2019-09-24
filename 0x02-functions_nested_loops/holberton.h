d75225a7b79d62c664927a36b26af4782ba2adee
mmanumos <mosqueramanuel5@gmail.com>
2019-09-24 05:39:12 +0000

    Load files

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,3 +1,51 @@
-#ifndef _SAMPLE_HEADER_H_
-#define _SAMPLE_HEADER_H_
+#ifndef _PUTCHAR_H_
+#define _PUTCHAR_H_
 int _putchar(char);
+#endif
+
+int pepito(int);
+
+#ifndef _PRINT_ALPHABET_H_
+#define _PRINT_ALPHABET_H_
+void print_alphabet(void);
+#endif
+
+#ifndef _PRINT_ALPHABET_X_10_H_
+#define _PRINT_ALPHABET_X_10_H_
+void print_alphabet_x10(void);
+#endif
+
+#ifndef _IS_LOWER_H_
+#define _IS_LOWER_H_
+int _islower(int c);
+#endif
+
+#ifndef _IS_ALPHA_H_
+#define _IS_ALPHA_H_
+int _isalpha(int c);
+#endif
+
+#ifndef _PRINT_SIGN_H_
+#define _PRINT_SIGN_H_
+int print_sign(int n);
+#endif
+
+#ifndef _ABS_H_
+#define _ABS_H_
+int _abs(int);
+#endif
+
+#ifndef _PRINT_LAST_DIGIT_H_
+#define _PRINT_LAST_DIGIT_
+int print_last_digit(int);
+#endif
+
+#ifndef _JACK_BAUER_H_
+#define _JACK_BAUER_H_
+void jack_bauer(void);
+#endif
+
+#ifndef _TIMES_TABLE_H_
+#define _TIMES_TABLE_H_
+void times_table(void);
+#endif
