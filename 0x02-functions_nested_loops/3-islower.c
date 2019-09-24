79881d76f9cb00b3dabb952844980009532e67fa
mmanumos <mosqueramanuel5@gmail.com>
2019-09-24 05:57:10 +0000

    fix - Function that checks for lowercase character.

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,15 @@
 #include "holberton.h"
 
 /**
- * print_alphabet_x10 - 10 times
- *
+ * _islower - is lower or no
+ * @c: variable
  * Return: Always 0
  */
 int _islower(int c)
 {
-        if(c >= 97 && c <= 122){
-		return(1);
-	}
-	else{
-		return(0);
-	}
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+
 }
