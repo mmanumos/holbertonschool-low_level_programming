23e6ec3af386c190ffb5896562b8108390612a39
mmanumos <mosqueramanuel5@gmail.com>
2019-09-24 18:29:41 +0000

    Function that adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * add - add
+ * @n1: number 1
+ * @n2: number 2
+ * Return: Always 0
+ */
+
+int add(int n1, int n2)
+{
+	int n;
+
+	n = (n1 + n2);
+	return (n);
+
+}
