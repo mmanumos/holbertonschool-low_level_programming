4a3766d7ec0a7425d78332d6d0553831f07fff64
mmanumos <mosqueramanuel5@gmail.com>
2019-09-26 16:09:11 +0000

    Function that checks for uppercase character.

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,87 @@
+#ifndef _MACRO_H_
+#define _MACRO_H_
+int _putchar(char);
+int _isupper(int c);
+#endif
+
+
+
+
+
+
+
+
+#ifndef _PRINT_ALPHABET_H_
+#define _PRINT_ALPHABET_H_
+void print_alphabet(void);
+#endif
+
+#ifndef _PRINT_ALPHABET_X_10_H_
+#define _PRINT_ALPHABET_X_10_H_
+void print_alphabet_x10(void);
+#endif
+
+#ifndef _IS_LOWER_H_
+#define _IS_LOWER_H_
+int _islower(int c);
+#endif
+
+#ifndef _IS_ALPHA_H_
+#define _IS_ALPHA_H_
+int _isalpha(int c);
+#endif
+
+#ifndef _PRINT_SIGN_H_
+#define _PRINT_SIGN_H_
+int print_sign(int n);
+#endif
+
+#ifndef _ABS_H_
+#define _ABS_H_
+int _abs(int);
+#endif
+
+#ifndef _PRINT_LAST_DIGIT_H_
+#define _PRINT_LAST_DIGIT_
+int print_last_digit(int);
+#endif
+
+#ifndef _JACK_BAUER_H_
+#define _JACK_BAUER_H_
+void jack_bauer(void);
+#endif
+
+#ifndef _TIMES_TABLE_H_
+#define _TIMES_TABLE_H_
+void times_table(void);
+#endif
+
+#ifndef _ADD_H_
+#define _ADD_H_
+int add(int, int);
+#endif
+
+#ifndef _PRINT_TO_98_
+#define _PRINT_TO_98_
+void print_to_98(int n);
+#endif
+
+#ifndef _PRINT_TIMES_TABLE_
+#define _PRINT_TIMES_TABLE_
+void print_times_table(int n);
+#endif
+
+#ifndef _MOD_RES_
+#define _MOD_RES_
+int mod_res(int res);
+#endif
+
+#ifndef _ADD_COMMA_
+#define _ADD_COMMA_
+void add_comma(int n, int num2);
+#endif
+
+#ifndef _ADD_SPACE_
+#define _ADD_SPACE_
+void add_space(int resul, int num2);
+#endif
