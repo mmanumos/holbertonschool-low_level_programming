847d756d5c98207e9988e2a9e3c8a1c3cc9d7e0a
mmanumos <mosqueramanuel5@gmail.com>
2019-10-01 16:19:13 +0000

    Function that reverses a string.

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _puts - print string
+ * @str: string
+ *
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_rev - print string
+ * @s: string
+ *
+ * Return: Always 0
+ */
+void print_rev(char *s)
+{
+	int ts;
+
+	for (ts = 0; s[ts] != '\0'; ts++)
+		;
+
+	while (ts >= 0)
+	{
+		_putchar(s[ts]);
+		ts--;
+	}
+	_putchar('\n');
+}
