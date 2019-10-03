b40baeb864c7cbee9276285767b6ac589351de5f
mmanumos <mosqueramanuel5@gmail.com>
2019-10-03 22:28:25 +0000

    From lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - upper character
+ * @cad: string
+ *
+ * Return: Return string to pointer
+ */
+
+char *string_toupper(char *cad)
+{
+	int cont;
+
+	for (cont = 0; cad[cont] != '\0'; cont++)
+	{
+		if (cad[cont] >= 97 && cad[cont] <= 122)
+			cad[cont] = cad[cont] - 32;
+	}
+	return (cad);
+}
