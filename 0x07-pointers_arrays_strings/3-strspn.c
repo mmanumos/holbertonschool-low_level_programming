16fb13dc16fc41500c164dd269cbd361f74d7e83
mmanumos <mosqueramanuel5@gmail.com>
2019-10-07 22:01:18 +0000

    Count maths of prefix into string

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strspn - count math of prefix of chain
+ * @s: pointer
+ * @accept: character to match
+ *
+ * Return: s
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int count, count2;
+
+	for (count = 0; s[count]; count++)
+	{
+		for (count2 = 0; accept[count2]; count2++)
+		{
+			if (s[count] == accept[count2])
+				break;
+		}
+
+		if (!accept[count2])
+			break;
+
+	}
+
+	return (count);
+}
