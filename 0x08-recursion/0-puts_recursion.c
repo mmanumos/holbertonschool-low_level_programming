d2ddcf058756a3676125eae2bb481e4dc41770fd
mmanumos <mosqueramanuel5@gmail.com>
2019-10-09 16:08:04 +0000

    "

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ *
+ * _puts_recursion - prints string using recursion
+ *
+ * @s : String to print
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+		return;
+	}
+}
---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,5 @@
+#ifndef _MACRO_H_
+#define _MACRO_H_
+int _putchar(char c);
+void _puts_recursion(char *s);
+#endif
