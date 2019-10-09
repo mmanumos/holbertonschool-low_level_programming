461d9ff7e1913bd0a7f40d88b4676738f28efbe5
mmanumos <mosqueramanuel5@gmail.com>
2019-10-09 22:52:51 +0000

    Get the power of a number using recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ *  _pow_recursion - get the value of x
+ * raised to the power of y
+ *
+ *
+ * @x : given number
+ * @y : power
+ *
+ * Return: the result of power
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, (y - 1)));
+	}
+
+}
