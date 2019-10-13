3811949b688677fa200bdaf37d4e3a5b9757b2f3
mmanumos <mosqueramanuel5@gmail.com>
2019-10-13 01:48:37 +0000

    Detecting the name of the program with argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - prints the name of program
+ *
+ * @argc: count pasts arguments
+ * @argv: array of pasts arguments
+ *
+ * Return: always 0
+ *
+ */
+
+int main(int argc __attribute__((__unused__)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/holberton.h
@@ -0,0 +1,4 @@
+#ifndef _MACRO_H_
+#define _MACRO_H_
+int _putchar(char c);
+#endif
