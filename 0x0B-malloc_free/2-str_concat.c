f095cd1e0645a89eb91cd00d1de4b79f8c8b9baf
mmanumos <mosqueramanuel5@gmail.com>
2019-10-15 22:47:14 +0000

    Changing validations

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,87 +1,68 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strlen - return length of a string
+ *
+ * @s: char type
+ * Return:  length of string
+ */
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+		;
+	return (a);
+}
+/**
+ * _strncat - function to concatnate strings with n bytes
+ *
+ * @dest: destination for concatnation
+ * @src: source of string
+ * @n: int type for size of byte
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len, a;
+
+	dest_len = _strlen(dest);
+	for (a = 0; a < n && src[a] != '\0'; a++)
+		dest[dest_len + a] = src[a];
+	return (dest);
+}
 
 /**
- * str_concat - concat two strings
- * using malloc to get the size with both string lengths
- *
- * @s1 : string1 to concatenate
- * @s2 : string2 to concatenate
- *
- * Return: fail-Null succes-str
+ * *str_concat - function to allocate space for sting concatnation
+ * @s1: array pointer to destination of string
+ * @s2: array pointer to source of string
+ * Return: return pointer to copy of string
  */
 
 char *str_concat(char *s1, char *s2)
 {
+	char *ptr;
+	int size1, size2;
 
-	int len1, len2, size;
-	(s1 == NULL) ? s1 = "" : s1;
-	(s2 == NULL) ? s2 = "" : s2;
-
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	size = (len1 + len2);
-
-	if ((size) > 0)
+	if (s1 == NULL)
 	{
-		char *dest = malloc(sizeof(char) * (size + 1));
-
-		if (dest == NULL)
-			return (NULL);
-
-		_strncat(dest, s1, len1);
-		_strncat(dest, s2, len2);
-		dest += '\0';
-		return (dest);
+		s1 = "";
 	}
-	else
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	size1 = _strlen(s1);
+	size2 = _strlen(s2);
+
+	ptr = malloc(((size1 + size2) + 1) * sizeof(char));
+	if (ptr == NULL)
 	{
 		return (NULL);
 	}
-}
-
-
-/**
- * _strlen - get the length
- * of string
- *
- * @s : string to measure
- *
- * Return: fail-Null succes-str
- */
-
-int _strlen(char *s)
-{
-	int cont;
-
-	for (cont = 0; s[cont] != '\0'; cont++)
-		;
-
-	return (cont);
-}
-
-
-
-/**
- * _strncat - appends string to other string
- * @dest: pointed array
- * @src: String to copy
- * @n: chars number
- * Return: Return string to pointer
- */
-
-
-char *_strncat(char *dest, char *src, int n)
-{
-	int st, count;
-
-	st = _strlen(dest);
-
-	for (count = 0; count < n && src[count] != '\0'; count++)
-	{
-		dest[st + count] = src[count];
-	}
-
-	return (dest);
+	_strncat(ptr, s1, size1);
+	_strncat(ptr, s2, size2);
+	ptr += '\0';
+	return (ptr);
 }
