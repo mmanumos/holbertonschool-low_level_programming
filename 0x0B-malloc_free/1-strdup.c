4d20eda1b696d2b82e8ccb3ed23fafa2e8d48152
mmanumos <mosqueramanuel5@gmail.com>
2019-10-15 20:03:25 +0000

    Copy string to other string using malloc and after free

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _strdup - copy a string to
+ * other string and after free it
+ *
+ * @str : string to copy
+ *
+ * Return: fail-Null succes-str
+ */
+
+char *_strdup(char *str)
+{
+
+	int len;
+
+	len = _strlen(str);
+
+	if (len > 0)
+	{
+		char *dest = malloc(sizeof(char) * len);
+
+		if (dest != NULL)
+		{
+			int i = 0;
+
+			while (i < len)
+			{
+				dest[i] = str[i];
+				i++;
+			}
+			return (dest);
+		}
+		else
+		{
+			return (NULL);
+		}
+	}
+	else
+	{
+		return (NULL);
+	}
+}
+
+
+/**
+ * _strlen - get the length
+ * of string
+ *
+ * @s : string to measure
+ *
+ * Return: fail-Null succes-str
+ */
+
+int _strlen(char *s)
+{
+	int cont;
+
+	for (cont = 0; s[cont] != '\0'; cont++)
+		;
+
+	return (cont);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -2,4 +2,6 @@
 #define _MACRO_H_
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+int _strlen(char *s);
 #endif
