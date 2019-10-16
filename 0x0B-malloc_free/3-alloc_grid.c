a78d7050c9d67b29f39b07b1e8e78dd278d98261
mmanumos <mosqueramanuel5@gmail.com>
2019-10-16 02:13:53 +0000

    fixing validation for second malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,7 +25,7 @@ int **alloc_grid(int width, int height)
 	for (c1 = 0; c1 < height; c1++)
 	{
 		array[c1] = malloc(width * sizeof(int));
-		if (array == NULL)
+		if (array[c1] == NULL)
 			return (NULL);
 
 		for (c2 = 0; c2 < width; c2++)
