3353c95366164364a9740415e19c83f71e0be52d
mmanumos <mosqueramanuel5@gmail.com>
2019-10-17 19:35:36 +0000

    Get an array with a minimum a maximum value

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * array_range - get an array
+ * with min and max
+ *
+ * @min: Minimum value
+ * @max: Maximum value
+ *
+ * Return: Faill-Null succes-array
+ */
+
+
+int *array_range(int min, int max)
+{
+	int *array, c1;
+
+	if ((min < max) && (min >= 0) && (min >= 0))
+	{
+		array = malloc(((max - min) + 1) * sizeof(int));
+
+		if (array != NULL)
+		{
+			for (c1 = 0; min <= max; c1++)
+			{
+				array[c1] = min++;
+			}
+			return (array);
+		}
+		else
+		{
+			return (NULL);
+		}
+	}
+	else
+	{
+		return (NULL);
+	}
+}
