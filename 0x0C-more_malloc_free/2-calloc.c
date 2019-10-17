4b3a8a271348e3ab63c91e9653ce9fde6e79e2b5
mmanumos <mosqueramanuel5@gmail.com>
2019-10-17 18:13:44 +0000

    Validation for negative input from size and nmemb

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,15 +18,23 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *array;
 	unsigned int c1;
 
-	array = malloc(nmemb * size);
-
-	if ((array != NULL) && (nmemb > 0) && (size > 0))
+	if ((nmemb > 0) && (size > 0))
 	{
-		for (c1 = 0; c1 < (size * nmemb); c1++)
+
+		array = malloc(nmemb * size);
+
+		if (array != NULL)
 		{
-			array[c1] = 0;
+			for (c1 = 0; c1 < (size * nmemb); c1++)
+			{
+				array[c1] = 0;
+			}
+			return (array);
 		}
-		return (array);
+		else
+		{
+			return (NULL);
+		}
 	}
 	else
 	{
