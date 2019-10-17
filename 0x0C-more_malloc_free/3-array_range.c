fd89ddcc7c5bdf0b3041eff1c2d6cf1325dc8c4c
mmanumos <mosqueramanuel5@gmail.com>
2019-10-17 20:25:22 +0000

    Remove validation for negative min and max

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,22 +17,20 @@ int *array_range(int min, int max)
 {
 	int *array, c1;
 
-	if ((min <= max) && (min >= 0) && (max >= 0))
+	if (min <= max)
 	{
 		array = malloc(((max - min) + 1) * sizeof(int));
 
-		if (array != NULL)
-		{
-			for (c1 = 0; min <= max; c1++)
-			{
-				array[c1] = min++;
-			}
-			return (array);
-		}
-		else
+		if (array == NULL)
 		{
 			return (NULL);
 		}
+
+		for (c1 = 0; min <= max; c1++)
+		{
+			array[c1] = min++;
+		}
+		return (array);
 	}
 	else
 	{
