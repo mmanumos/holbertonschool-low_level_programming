a28942db0f059150aa4e72d8dda304d336a04957
mmanumos <mosqueramanuel5@gmail.com>
2019-10-21 16:47:18 +0000

    Struct to dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - struct to save dogs
+ *
+ * @name: name's dog
+ * @age:  age's dog
+ * @owner: owner's dog
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
