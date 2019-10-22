2ae457087a8e57da73f1f8359c8464ba8d3783ac
mmanumos <mosqueramanuel5@gmail.com>
2019-10-22 00:17:23 +0000

    Fixing validations

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,38 +18,62 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
 	char *newname, *newowner;
-	int c1;
 
 	d = malloc(sizeof(dog_t));
 
 	if (d != NULL)
 	{
 		newname = malloc(sizeof(name));
-		newowner = malloc(sizeof(owner));
 
-		if ((newname != NULL) && (newowner != NULL))
+		if (newname == NULL)
 		{
-			for (c1 = 0; *name; c1++, ++name)
-				newname[c1] = *name;
-
-			newname[c1 + 1] = '\0';
-
-			for (c1 = 0; *owner; c1++, ++owner)
-				newowner[c1] = *owner;
-
-			newowner[c1 + 1] = '\0';
-
-			d->name = newname;
-			d->age = age;
-			d->owner = newowner;
-
-			return (d);
-
+			free(d);
+			return (NULL);
 		}
 		else
+		{
+			newowner = malloc(sizeof(owner));
+		}
+
+		if (newowner == NULL)
+		{
+			free(d);
+			free(newname);
 			return (NULL);
+		}
+
+		newname = _strcpy(newname, name);
+		newowner = _strcpy(newowner, owner);
+
+		d->name = newname;
+		d->age = age;
+		d->owner = newowner;
+
+		return (d);
+
 	}
 	else
 		return (NULL);
 
+}
+
+
+/**
+ * _strcpy - reset values from from pointer
+ * @dest: pointed array
+ * @src: String to copy
+ *
+ * Return: Return string to pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int cont = 0;
+
+	while (src[cont] != '\0')
+	{
+		dest[cont] = src[cont];
+		cont++;
+	}
+	dest[cont] = '\0';
+	return (dest);
 }
