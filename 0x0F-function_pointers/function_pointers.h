2121c15cf6fe6154a6ae340413f15f0748e10033
mmanumos <mosqueramanuel5@gmail.com>
2019-10-23 23:01:32 +0000

    Adding prototypes to main header

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -11,6 +11,14 @@ int int_index(int *array, int size, int (*cmp)(int));
 
 int (*get_op_func(char *s))(int, int);
 
+int op_add(int a, int b);
 
+int op_sub(int a, int b);
+
+int op_mul(int a, int b);
+
+int op_div(int a, int b);
+
+int op_mod(int a, int b);
 
 #endif
