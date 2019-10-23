395bed3468282d2fe6ab540f02a6d3a8a49ffb60
mmanumos <mosqueramanuel5@gmail.com>
2019-10-23 23:08:47 +0000

    [Fix] Adding prototype to 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -16,9 +16,15 @@ char *op;
 
 
 int op_add(int a, int b);
+
 int op_sub(int a, int b);
+
 int op_mul(int a, int b);
+
 int op_div(int a, int b);
+
 int op_mod(int a, int b);
+
+int (*get_op_func(char *s))(int, int);
 
 #endif
