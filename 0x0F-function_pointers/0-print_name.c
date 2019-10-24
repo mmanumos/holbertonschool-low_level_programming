504c020b8a2c5df7f4c0794727ed2e16695c6ca4
mmanumos <mosqueramanuel5@gmail.com>
2019-10-24 15:32:16 +0000

    Re-pushing 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - print string
+ * using the given pointer to function
+ *
+ * @name:String to print
+ * @f: pointer to function
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if ((f != NULL) && (name != NULL))
+		f(name);
+}
