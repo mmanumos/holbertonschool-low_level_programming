44e546ba48f7226d5f7dc05fe66d664c4021d2c9
mmanumos <mosqueramanuel5@gmail.com>
2019-10-25 15:07:56 +0000

    [Feature] file witj function to return a sum New file: 0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - return sum of all arguments
+ *
+ * @n: undefined numbers of parameters
+ *
+ * Return: fail-0 success-sum
+ */
+
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list my_valist;
+	unsigned int sum = 0, i;
+
+	if (n > 0)
+	{
+		va_start(my_valist, n);
+
+		for (i = 0; i < n; i++, sum += va_arg(my_valist, unsigned int))
+			;
+
+		va_end(my_valist);
+	}
+
+	return (sum);
+}
