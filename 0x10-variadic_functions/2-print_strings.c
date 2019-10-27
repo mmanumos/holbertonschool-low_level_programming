c94fe14e348e2b0cdf8e194f434c1448fc560aa5
mmanumos <mosqueramanuel5@gmail.com>
2019-10-27 00:01:31 +0000

    [Feature] Print strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - return sum of all arguments
+ *
+ * @separator: undefined numbers of parameters
+ * @n:number of elements
+ *
+ * Return: fail-0 success-sum
+ */
+
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+
+	va_list par;
+	unsigned int i;
+	char temp[100];
+
+	if (n < 1)
+		return;
+
+	va_start(par, n);
+
+	for (i = 0; i <= n; i++)
+	{
+		temp = va_arg(par, char *);
+		printf("%s", (temp  != NULL) ? temp : "(nil)");
+
+		if ((i != n) && (separator != NULL))
+			printf("%s", separator);
+
+		if (i == n)
+			printf("%c", '\n');
+	}
+
+	va_end(par);
+}
