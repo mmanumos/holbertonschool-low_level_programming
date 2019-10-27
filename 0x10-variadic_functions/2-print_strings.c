25458d55e38ee1b102ab5772f9824ecbe949a9da
mmanumos <mosqueramanuel5@gmail.com>
2019-10-27 00:53:16 +0000

    [Fix] Changing datatype for temp

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,14 +15,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_list par;
 	unsigned int i;
-	char temp[100];
+	char *temp;
 
 	if (n < 1)
 		return;
 
 	va_start(par, n);
 
-	for (i = 0; i <= n; i++)
+	for (i = 1; i <= n; i++)
 	{
 		temp = va_arg(par, char *);
 		printf("%s", (temp  != NULL) ? temp : "(nil)");
