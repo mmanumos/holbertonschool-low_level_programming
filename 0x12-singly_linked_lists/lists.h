07e7216f7f39f5b40dfc90e1ce0ba1bba32d0835
mmanumos <mosqueramanuel5@gmail.com>
2019-10-31 15:21:14 +0000

    [Feature] Adding base structure list_s to header modified:   lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,4 +1,22 @@
 #ifndef LISTS_H_
 #define LISTS_H_
 int _putchar(char c);
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+
 #endif
