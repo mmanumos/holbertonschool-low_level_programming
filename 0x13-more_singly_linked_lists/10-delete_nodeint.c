d9f5795c09aaaf244e33afda38ab31e54183c37b
mmanumos <mosqueramanuel5@gmail.com>
2019-11-05 16:36:33 +0000

    [Feature] It allows delete a node in a given position. new file:   10-delete_nodeint.c modified:   lists.h

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - Delete a node
+ * in the given position
+ *
+ * @head: head of list
+ * @index: position to delete the node
+ *
+ * Return: The new node
+ *
+ */
+
+
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+
+	listint_t *temp;
+	listint_t *before = *head;
+	unsigned int ln = 0;
+
+	if (!*head)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = before->next;
+		free(before);
+	}
+	else
+	{
+		while (ln < (index - 1) && before->next)
+		{
+			before = before->next;
+			ln++;
+		}
+
+		temp = before->next;
+		before->next = before->next->next;
+		free(temp);
+		return (1);
+
+	}
+	return (-1);
+}
