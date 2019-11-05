3c1e514a95e27bc69922fb47c3f96d79802e68d7
mmanumos <mosqueramanuel5@gmail.com>
2019-11-05 00:59:43 +0000

    [Fix] Adding validation for valgrind. modified:   4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -14,14 +14,12 @@
 
 void free_listint(listint_t *head)
 {
-	if(head == NULL)
-	{
-		return;
-	}
+	listint_t *next;
 
-	while (head)
+	while (head != NULL)
 	{
+		next = head->next;
 		free(head);
-		head = head->next;
+		head = next;
 	}
 }
