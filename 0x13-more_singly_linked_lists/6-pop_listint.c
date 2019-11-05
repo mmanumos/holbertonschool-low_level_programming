6e0d6bdb9db389757922fb9563481f6f45e80d32
mmanumos <mosqueramanuel5@gmail.com>
2019-11-05 02:03:53 +0000

    [Feature] It allows remove the head of list. new file:   6-pop_listint.c modified:   lists.h

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * pop_listint - remove
+ * the last node
+ *
+ * @head: head of list
+ *
+ * Return: element n of last node
+ *
+ */
+
+
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int ln = 0;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+
+	temp = *head;
+	ln = temp->n;
+	*head = temp->next;
+	free(temp);
+	return (ln);
+}
