e8c1afc0a47e19038d9c8460b8b05004e61001ea
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-04-16 23:32:30 +0000

    [Feature] search_binary algorithm to find a value.
    new file:   1-binary.c

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,62 @@
+#include "search_algos.h"
+
+
+/**
+ * print_array - Function to print contents of array
+ * @array: Pointer to array to be printed
+ * @size: size of array to be printed of type size_t
+ * Return: always successful
+ */
+
+void print_array(int *array, size_t size)
+{
+	size_t idx;
+
+	printf("Searching in array: ");
+	for (idx = 0; idx < size; idx++)
+	{
+		if (idx > 0 && idx < size)
+			printf(", ");
+		printf("%d", array[idx]);
+	}
+	printf("\n");
+}
+
+
+
+/**
+ * binary_search - Search into theu list the given value.
+ *
+ * @array: list
+ * @size: size of array
+ * @value: value to match
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t start;
+	size_t mid;
+	size_t end;
+
+	if (array)
+	{
+		start = 0;
+		end = size - 1;
+		while (start <= end)
+		{
+			print_array(array + start, end + 1 - start);
+			mid = (start + end) / 2;
+			if (array[mid] < value)
+				start = mid + 1;
+			else if (array[mid] > value)
+				end = mid - 1;
+			else
+				return (mid);
+		}
+	}
+	return (-1);
+}
+
+
