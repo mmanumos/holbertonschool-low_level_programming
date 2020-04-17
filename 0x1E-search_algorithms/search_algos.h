115ebe42bbccc055a578057584e88f73e3333089
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-04-17 02:21:45 +0000

    [Feature] Library is added. 	deleted:    1-binary modified:   search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,5 +4,8 @@
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
+void print_array(int *array, size_t size);
+int binary_search(int *array, size_t size, int value);
+
 
 #endif
